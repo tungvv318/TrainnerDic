6fa946a15c802346509beb3e8dbbb27da5fc6198
phuctd99 <43601776+phuctd99@users.noreply.github.com>
2018-10-04 23:02:33 +0700

    Update DictionaryManagement.java

--- Dictionary_Java_oop/src/dictionary/DictionaryManagement.java
+++ Dictionary_Java_oop/src/dictionary/DictionaryManagement.java
@@ -6,12 +6,13 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 import dictionary.Word;
 
 public class DictionaryManagement {
 	Scanner sc = new Scanner(System.in);
-	
+	//ham nhap tu man hinh
 	public  void insertFromCommandline(){
 		System.out.println("Nhap so luong tu: ");
 		int numberWord = sc.nextInt();
@@ -26,18 +27,76 @@ public class DictionaryManagement {
 			Dictionary.listWord.add(word);
 		}
 	}
+	//ham tim kiem
 	public void dictionaryLookup(){
 		System.out.println("nhap tu can tra: ");
 		String wordLookup = sc.nextLine();
 		for (int i = 0; i < Dictionary.listWord.size(); i++) {
 			Word word = Dictionary.listWord.get(i);
 			if((word.getWord_target()).equals(wordLookup)){
-				System.out.println("Find sucess!");
+				System.out.println("Da tim thay!");
 				System.out.println(word.getWord_target()+"         "+word.getWord_explain());
 			}
-			//thieu
+			else{
+				System.out.println("Khong tim thay!");
+			}
 		}
 	}
+	//them tu
+	public void addWord(){
+		System.out.println("Nhap tu can them: ");
+		String wordAdd =sc.nextLine();
+		System.out.println("Giai nghia: ");
+		String wordAddMean =sc.nextLine();
+		Dictionary.listWord.add(new Word(wordAdd,wordAddMean));
+		System.out.println("Them thanh cong");
+	}
+	//xoa tu
+	public void deleteWord(){
+		System.out.println("Nhap tu muon xoa: ");
+		String wordDelete = sc.nextLine();
+		for (int i = 0; i < Dictionary.listWord.size(); i++) {
+			Word word = Dictionary.listWord.get(i);
+			if(word.getWord_target().equals(wordDelete)){
+				Dictionary.listWord.remove(word);
+			}
+			}
+		System.out.println("Xoa thanh cong!");
+	}
+	//sua tu
+	public void editWord(){
+		System.out.println("Nhap tu muon thay the: ");
+		String replaceWord = sc.nextLine();
+		for(int i=0; i<Dictionary.listWord.size(); i++){
+			Word word = Dictionary.listWord.get(i);
+			if(word.getWord_target().equals(replaceWord)){
+				System.out.println("Nhap tu thay the: ");
+				String editWord = sc.nextLine();
+				System.out.println("Giai nghia: ");
+				String editWordMean = sc.nextLine();
+				Dictionary.listWord.set(i, new Word(editWord, editWordMean));
+			}
+			System.out.println("Sua thanh cong!");
+		}
+	}
+	//ham tim kiem cac tu
+	public void dictionarySearcher(){
+		System.out.println("Nhap tu can tim: ");
+		String wordSearch = sc.nextLine();
+		ArrayList<Word> newList = new ArrayList<Word>();
+		for(int i=0; i<Dictionary.listWord.size(); i++){
+			Word word = Dictionary.listWord.get(i);
+			if(word.getWord_target().indexOf(wordSearch) == 0){
+				newList.add(word);
+			}
+		}
+		int i=1;
+		for(Word word : newList){
+			System.out.printf(i+" %-15s%-15s\n", word.getWord_target(), word.getWord_explain());
+			i++;
+		}
+	}
+	//ham nhap tu file
 	public void insertFromFile(){
 		try {
 			FileInputStream fis = new FileInputStream("Dictionary.txt");
@@ -59,6 +118,7 @@ public class DictionaryManagement {
 			e.printStackTrace();
 		}
 	}
+	//ham xuat ra file
 	public void dictionaryExportToFile(){
 		try {
 			FileOutputStream fos = new FileOutputStream("OutputFile.txt");
