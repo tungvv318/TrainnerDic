46c1e51d8c1f074f7d6dceb3ed32265e86f56d02
phuctd99 <43601776+phuctd99@users.noreply.github.com>
2018-10-04 23:04:11 +0700

    Update DictionaryCommandline.java

--- Dictionary_Java_oop/src/dictionary/DictionaryCommandline.java
+++ Dictionary_Java_oop/src/dictionary/DictionaryCommandline.java
@@ -1,10 +1,13 @@
 package dictionary;
 import dictionary.DictionaryManagement;
 
+import java.util.Scanner;
+
 import dictionary.Dictionary;
 
 public class DictionaryCommandline {
 	DictionaryManagement dictionarymanagement = new DictionaryManagement();
+	Scanner sc =new Scanner(System.in);
 	public  void showAllWord(){
 		System.out.println("|No   |English   |Vietnamese");
 		for (int i = 0; i < Dictionary.listWord.size(); i++) {
@@ -20,13 +23,33 @@ public class DictionaryCommandline {
 	public void dictionaryAdvanced(){
 		//dictionarymanagement.insertFromCommandline();
 		dictionarymanagement.insertFromFile();
+		System.out.println("Choose: ");
+		System.out.println("1.Add Word ");
+		System.out.println("2.Delete Word ");
+		System.out.println("3.edit Word ");
+		System.out.println("4.Find Word");
+		int x = sc.nextInt();
+		sc.nextLine();
+		switch(x){
+			case 1: 
+				dictionarymanagement.addWord();
+				break;
+			case 2:
+				dictionarymanagement.deleteWord();
+				break;
+			case 3:
+				dictionarymanagement.editWord();
+				break;
+			case 4:
+				dictionarymanagement.dictionarySearcher();
+				break;
+				
+		}
 		dictionarymanagement.dictionaryExportToFile();
-		showAllWord();
-		dictionarymanagement.dictionaryLookup();
+		//showAllWord();
 	}
 	public static void main(String [] args){
 		DictionaryCommandline dicCommand = new  DictionaryCommandline();
 		dicCommand.dictionaryAdvanced();
 	}
-	
 }
